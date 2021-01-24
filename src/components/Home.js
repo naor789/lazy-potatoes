1122eab785038e2c4198184b4baa4a9d01ef5dbb
naor798 <naorkahalany@gmail.com>
2021-01-24 10:31:06 +0200

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import backgroundD from '../img/backgroundD.png'
+import imgA from '../img/imgA.png'
+import imgD from '../img/imgD.png'
 import AllTrips from './AllTrips';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -15,7 +16,8 @@ export default function Home() {
             <div className="container homePage">
                 <h1 className="home-heading">Welcome </h1>
                 <p className="home-text">There are many variations of passages of Lorem Ipsum available,<br />but the majority have suffered alteration in some form, by injected<br /> humour, or randomised words which don't look even slightly <br />believable. If you are going to use a passage of Lorem Ipsum, <br />you need to be sure there isn't anything embarrassing <br />hidden in the middle of text. </p>
-                <span><img src={backgroundD} alt="man and a woman" className="img" width="80%" /></span>
+                <span><img src={imgA} alt="man and a woman" className="imgA" width="70%" /></span>
+                <span><img src={imgD} alt="man and a woman" className="imgD" width="70%" /></span>
             <Link to='/alltrips'>
                 <Button className="home-button-all"> All trips  </Button>
             </Link>
