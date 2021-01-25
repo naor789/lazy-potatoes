302667aca4066ba83562848132fd6bf942e5dc63
naor798 <naorkahalany@gmail.com>
2021-01-25 22:03:53 +0200

    modal css

--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
-import TripCardModal from './TripCardModal'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import TripCardModal from "./TripCardModal";
+import "./tripCard.css";
 
 export default function TripCard(props) {
-  const [modalShow, setModalShow] = React.useState(false)
+  const [modalShow, setModalShow] = React.useState(false);
 
-  const { id, area, time, store, quantity, name } = props.trip
+  const { id, area, time, store, quantity, name } = props.trip;
 
   console.log(props.trip);
   return (
-    <Card className='m-3 tripCard' style={{ width: '18rem' }} key={id}>
+    <Card className="m-3 tripCard" style={{ width: "18rem" }} key={id}>
       <Card.Body>
         <Card.Title>{name}'s trip</Card.Title>
-        <Card.Subtitle className='mb-2 text-muted'>Area: {area}</Card.Subtitle>
-        <Card.Text>Store {store}</Card.Text>
-        <Card.Text>Time: {time}</Card.Text>
-        <Card.Text>Able to carry {quantity}</Card.Text>
-        <Button onClick={() => setModalShow(true)}>Make a Request</Button>
+        <Card.Subtitle className="mb-2 text-muted">Area: {area}</Card.Subtitle>
+        <Card.Text>
+          Store: <b>{store}</b>
+        </Card.Text>
+        <Card.Text>
+          Time: <b>{time}</b>
+        </Card.Text>
+        <Card.Text>
+          Able to carry: <b>{quantity}</b>
+        </Card.Text>
+        <Button onClick={() => setModalShow(true)} className="buttons">
+          Make a Request
+        </Button>
         <TripCardModal
           trip={props.trip}
           show={modalShow}
@@ -24,5 +33,5 @@ export default function TripCard(props) {
         />
       </Card.Body>
     </Card>
-  )
+  );
 }
--- src/components/TripCardModal.jsx
+++ src/components/TripCardModal.jsx
@@ -1,105 +1,162 @@
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Alert from 'react-bootstrap/Alert';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
+import "./tripCardModal.css";
+
+
 
 export default function TripCardModal(props) {
-	//get the trip doc from the db, and axios.put to add the request
-	const { id, area, time, store, quantity } = props.trip;
-	const [requestList, setRequestList] = useState('');
-	const [reqItem1, setReqItem1] = useState('');
-	const [reqItem2, setReqItem2] = useState('');
-	const [reqItem3, setReqItem3] = useState('');
-	const [reqDropOff, setReqDropOff] = useState('');
-	const [error, setError] = useState(false);
+  //get the trip doc from the db, and axios.put to add the request
+  const { id, area, time, store, quantity } = props.trip;
+  const [requestList, setRequestList] = useState("");
+  const [reqItem1, setReqItem1] = useState("");
+  const [reqItem2, setReqItem2] = useState("");
+  const [reqItem3, setReqItem3] = useState("");
+  const [reqDropOff, setReqDropOff] = useState("");
+  const [error, setError] = useState(false);
 
-	function sendRequest() {
-		if (!reqItem1 || !reqDropOff) {
-			setError(true);
-			return;
-		}
-		const requestObject = [
-			{
-				id,
-				reqItem1,
-				reqItem2,
-				reqItem3,
-				reqDropOff,
-			},
-		];
-		setRequestList(requestObject);
-		console.log(requestObject);
-		props.onHide();
-	}
-	console.log(requestList);
-	return (
-		<Modal
-			{...props}
-			size='lg'
-			aria-labelledby='contained-modal-title-vcenter'
-			centered>
-			<Alert
-				show={error}
-				variant='danger'
-				onClose={() => setError(false)}
-				dismissible>
-				<Alert.Heading>Oh no! You got an error 🙀</Alert.Heading>
-				<p>
-					You must fill out at least one requested item and your information in
-					order to send a request...
-				</p>
-			</Alert>
-			<Modal.Header closeButton>
-				<Modal.Title id='contained-modal-title-vcenter'>
-					Juliet's trip in {area}
-				</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<p>
-					Store {store} Time: {time}
-				</p>
-				<h4>Username is willing to carry {quantity} items</h4>
-				<p>Please state the Items you need</p>
-				<div className='request'>
-					<input
-						type='string'
-						name='reqItem1'
-						onChange={e => setReqItem1(e.target.value)}
-					/>
-					<input
-						type='string'
-						name='reqItem2'
-						onChange={e => setReqItem2(e.target.value)}
-					/>
-					<input
-						type='string'
-						name='reqItem3'
-						onChange={e => setReqItem3(e.target.value)}
-					/>
+  function sendRequest() {
+    if (!reqItem1 || !reqDropOff) {
+      setError(true);
+      return;
+    }
+    const requestObject = [
+      {
+        id,
+        reqItem1,
+        reqItem2,
+        reqItem3,
+        reqDropOff,
+      },
+    ];
+    setRequestList(requestObject);
+    console.log(requestObject);
+    props.onHide();
+  }
+  console.log(requestList);
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Alert
+        show={error}
+        variant="danger"
+        onClose={() => setError(false)}
+        dismissible
+      >
+        <Alert.Heading>Oh no! You got an error 🙀</Alert.Heading>
+        <p>
+          You must fill out at least one requested item and your information in
+          order to send a request...
+        </p>
+      </Alert>
+      <Modal.Header closeButton>
+        <Button variant="light"  type="button">
+          x
+        </Button>
 
-					<p>Drop Off Location</p>
-					<input
-						type='string'
-						name='reqDropOff'
-						onChange={e => setReqDropOff(e.target.value)}
-					/>
-					<Button onClick={sendRequest}>Send Request</Button>
-				</div>
-			</Modal.Body>
-			<Modal.Footer>
-				{requestList && (
-					<div>
-						{requestList.map(req => (
-							<div key={req.id} req={req.requestObject}>
-								items: {reqItem1}
-								drop-off: {reqDropOff}
-								<Button>Accept</Button>
-								<Button>Refuse</Button>
-							</div>
-						))}
-					</div>
-				)}
-			</Modal.Footer>
-		</Modal>
-	);
+        <Modal.Title id="contained-modal-title-vcenter">
+          Juliet's trip in <b>{area}</b>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div>
+          Store: <b>{store}</b> Time:<b> {time}</b>
+        </div>
+        <div>
+          Username is willing to carry <b> {quantity} </b>items
+        </div>
+        {/* <div>Please state the Items you need</div> */}
+        <Form>
+          <Form.Group id="request">
+            <Form.Label>Please state the Items you need</Form.Label>
+            <Row>
+              <Col>
+                <Form.Control
+                  type="string"
+                  required
+                  name="reqItem1"
+                  onChange={(e) => setReqItem1(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  type="string"
+                  name="reqItem2"
+                  onChange={(e) => setReqItem2(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  type="string"
+                  name="reqItem3"
+                  onChange={(e) => setReqItem3(e.target.value)}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Label>Drop Off Location</Form.Label>
+                <Form.Control
+                  type="string"
+                  required
+                  name="reqDropOff"
+                  onChange={(e) => setReqDropOff(e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Button onClick={sendRequest} className="request-button">
+                  Send Request
+                </Button>
+              </Col>
+            </Row>
+          </Form.Group>
+        </Form>
+
+        {/* <div className="request">
+          <input
+            type="string"
+            name="reqItem1"
+            onChange={(e) => setReqItem1(e.target.value)}
+          />
+          <input
+            type="string"
+            name="reqItem2"
+            onChange={(e) => setReqItem2(e.target.value)}
+          />
+          <input
+            type="string"
+            name="reqItem3"
+            onChange={(e) => setReqItem3(e.target.value)}
+          />
+
+          <p>Drop Off Location</p>
+          <input
+            type="string"
+            name="reqDropOff"
+            onChange={(e) => setReqDropOff(e.target.value)}
+          /> */}
+        {/* <Button onClick={sendRequest}>Send Request</Button> */}
+        {/* </div> */}
+      </Modal.Body>
+      {requestList && (
+        <Modal.Footer>
+          <div>
+            {requestList.map((req) => (
+              <div key={req.id} req={req.requestObject}>
+                items: {reqItem1}
+                drop-off: {reqDropOff}
+                <Button>Accept</Button>
+                <Button>Refuse</Button>
+              </div>
+            ))}
+          </div>
+        </Modal.Footer>
+      )}
+    </Modal>
+  );
 }
