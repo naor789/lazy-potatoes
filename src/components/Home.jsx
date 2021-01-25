0b22036467d285b85817f10da6ff450dcca7ab1a
julogav <julietgavison@gmail.com>
2021-01-25 14:56:31 +0200

    commented on what to do next

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import imgA from '../img/imgA.png'
-import imgD from '../img/imgD.png'
+import React from 'react';
+import imgA from '../img/imgA.png';
+import imgD from '../img/imgD.png';
 import AllTrips from './AllTrips';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-
-
-
 export default function Home() {
-
-    return (
-        <>
-
-            <div className="container homePage">
-                <h1 className="home-heading">Welcome </h1>
-                <p className="home-text">There are many variations of passages of Lorem Ipsum available,<br />but the majority have suffered alteration in some form, by injected<br /> humour, or randomised words which don't look even slightly <br />believable. If you are going to use a passage of Lorem Ipsum, <br />you need to be sure there isn't anything embarrassing <br />hidden in the middle of text. </p>
-                <span><img src={imgA} alt="man and a woman" className="imgA" width="70%" /></span>
-                <span><img src={imgD} alt="man and a woman" className="imgD" width="70%" /></span>
-            <Link to='/alltrips'>
-                <Button className="home-button-all"> All trips  </Button>
-            </Link>
-            <Link to='/add-trip'>
-                    <Button className="home-button-new"> Add a new trip </Button>
-            </Link>
-
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className='container homePage'>
+				<h1 className='home-heading'>Welcome Username</h1>
+				<p className='home-text'>
+					There are many variations of passages of Lorem Ipsum available,
+					<br />
+					but the majority have suffered alteration in some form, by injected
+					<br /> humour, or randomised words which don't look even slightly{' '}
+					<br />
+					believable. If you are going to use a passage of Lorem Ipsum, <br />
+					you need to be sure there isn't anything embarrassing <br />
+					hidden in the middle of text.{' '}
+				</p>
+				<span>
+					<img src={imgA} alt='man and a woman' className='imgA' width='70%' />
+				</span>
+				<span>
+					<img src={imgD} alt='man and a woman' className='imgD' width='70%' />
+				</span>
+				<Link to='/alltrips'>
+					<Button className='home-button-all'> All trips </Button>
+				</Link>
+				<Link to='/add-trip'>
+					<Button className='home-button-new'> Add a new trip </Button>
+				</Link>
+			</div>
+		</>
+	);
 }
--- src/components/TripCard.jsx
+++ src/components/TripCard.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
-import { Button , Card} from 'react-bootstrap'
-import TripCardModal from './TripCardModal'
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import TripCardModal from './TripCardModal';
 
 export default function TripCard(props) {
+	const [modalShow, setModalShow] = React.useState(false);
+	const { id, area, time, store, quantity } = props.trip;
 
-  const [modalShow, setModalShow] = React.useState(false)
-  const { id, area, time, store, container, quantity } = props.trip
-
-  return (
-      <Card className="m-3 tripCard" style={{ width: "18rem" }} key={id}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            Area: {area}
-        </Card.Subtitle>
-          <Card.Text>Store {store}</Card.Text>
-          <Card.Text>Time: {time}</Card.Text>
-          <Card.Text>
-            Able to carry {quantity}
-          </Card.Text>
-          <Button onClick={() => setModalShow(true)}>Make a Request</Button>
-          <TripCardModal 
-          trip={props.trip}
-          show={modalShow} 
-          onHide={() => setModalShow(false)} />
-        </Card.Body>
-      </Card>
-  );
-
+	return (
+		<Card className='m-3 tripCard' style={{ width: '18rem' }} key={id}>
+			<Card.Body>
+				<Card.Title>User's trip</Card.Title>
+				<Card.Subtitle className='mb-2 text-muted'>Area: {area}</Card.Subtitle>
+				<Card.Text>Store {store}</Card.Text>
+				<Card.Text>Time: {time}</Card.Text>
+				<Card.Text>Able to carry {quantity}</Card.Text>
+				<Button onClick={() => setModalShow(true)}>Make a Request</Button>
+				<TripCardModal
+					trip={props.trip}
+					show={modalShow}
+					onHide={() => setModalShow(false)}
+				/>
+			</Card.Body>
+		</Card>
+	);
 }
