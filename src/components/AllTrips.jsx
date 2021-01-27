e9449ef3ea6099073dc9089b8b0a8a69d4de9630
naor798 <naorkahalany@gmail.com>
2021-01-27 14:35:44 +0200

    home page

--- src/components/AllTrips.jsx
+++ src/components/AllTrips.jsx
@@ -1,76 +1,76 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Row, Form, Button } from 'react-bootstrap';
-import TripCard from './TripCard';
-import React from 'react';
-import { areas } from './add-trip';
-import { baseURL } from '../App';
-import './allTrips.css';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { Row, Form, Button } from 'react-bootstrap';
+// import TripCard from './TripCard';
+// import React from 'react';
+// import { areas } from './add-trip';
+// import { baseURL } from '../App';
+// import './allTrips.css';
 
-export default function AllTrips() {
-	const [allTrips, setAllTrips] = useState([]);
-	const [areaTrips, setAreaTrips] = useState([]);
-	const [searchArea, setSearchArea] = useState();
+// export default function AllTrips() {
+// 	const [allTrips, setAllTrips] = useState([]);
+// 	const [areaTrips, setAreaTrips] = useState([]);
+// 	const [searchArea, setSearchArea] = useState();
 
-	const handleSearch = async (event) => {
-		event.preventDefault();
-		const newSearch = {
-			searchArea: searchArea,
-		};
-		const res = await axios.get(
-			`http://localhost:5000/api/search?area=${searchArea}`
-		);
-		setAreaTrips(res.data);
-	};
+// 	const handleSearch = async (event) => {
+// 		event.preventDefault();
+// 		const newSearch = {
+// 			searchArea: searchArea,
+// 		};
+// 		const res = await axios.get(
+// 			`http://localhost:5000/api/search?area=${searchArea}`
+// 		);
+// 		setAreaTrips(res.data);
+// 	};
 
-	useEffect(() => {
-		const showAllTrips = async () => {
-			const res = await axios.get(`${baseURL}/api/alltrips`);
-			setAllTrips(res.data);
-		};
-		showAllTrips();
-	}, [allTrips]);
+// 	useEffect(() => {
+// 		const showAllTrips = async () => {
+// 			const res = await axios.get(`${baseURL}/api/alltrips`);
+// 			setAllTrips(res.data);
+// 		};
+// 		showAllTrips();
+// 	}, [allTrips]);
 
-	return (
-		<>
-			<Form>
-				<Form.Group controlId='Type'>
-					<Form.Label className='mt-1'>Search for Area</Form.Label>
-					<Form.Control
-						as='select'
-						defaultValue='Glilot'
-						value={searchArea}
-						required
-						//   name="type"
-						onChange={e => setSearchArea(e.target.value)}>
-						{areas.map(area => (
-							<option key={area.id} area={{ area }}>
-								{area}
-							</option>
-						))}
-					</Form.Control>
-					<Button onClick={handleSearch} className='button w-100' type='submit'>
-						Search
-					</Button>
-				</Form.Group>
-			</Form>
-			<div className='container mb-5 '>
-				<h1>All Trips</h1>
-				{/* {allTrips && (
-					<Row className='m-3'>
-						{allTrips.map(trip => (
-							<TripCard key={trip.id} trip={trip} />
-						))}
-					</Row> */}
-				{/* )} */}
-				{searchArea && (
-					<Row className='m-3'>
-						{searchArea.map(trip => (
-							<TripCard key={trip.id} trip={trip} />
-						))}
-					</Row>
-				)}
-			</div>
-		</>
-	);
-}
+// 	return (
+// 		<>
+// 			<Form>
+// 				<Form.Group controlId='Type'>
+// 					<Form.Label className='mt-1'>Search for Area</Form.Label>
+// 					<Form.Control
+// 						as='select'
+// 						defaultValue='Glilot'
+// 						value={searchArea}
+// 						required
+// 						//   name="type"
+// 						onChange={e => setSearchArea(e.target.value)}>
+// 						{areas.map(area => (
+// 							<option key={area.id} area={{ area }}>
+// 								{area}
+// 							</option>
+// 						))}
+// 					</Form.Control>
+// 					<Button onClick={handleSearch} className='button w-100' type='submit'>
+// 						Search
+// 					</Button>
+// 				</Form.Group>
+// 			</Form>
+// 			<div className='container mb-5 '>
+// 				<h1>All Trips</h1>
+// 				{/* {allTrips && (
+// 					<Row className='m-3'>
+// 						{allTrips.map(trip => (
+// 							<TripCard key={trip.id} trip={trip} />
+// 						))}
+// 					</Row> */}
+// 				{/* )} */}
+// 				{searchArea && (
+// 					<Row className='m-3'>
+// 						{searchArea.map(trip => (
+// 							<TripCard key={trip.id} trip={trip} />
+// 						))}
+// 					</Row>
+// 				)}
+// 			</div>
+// 		</>
+// 	);
+// }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,56 @@
-import React from 'react';
+import React from "react";
 // import imgA from "../img/imgA.png";
 // import imgD from "../img/imgD.png";
-import AllTrips from '../components/AllTrips/AllTrips';
-import { Button, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from '../contexts/UserContext';
+import AllTrips from "../components/AllTrips/AllTrips";
+import { Button, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 export default function Home() {
-	const { currentUser } = useContext(UserContext);
-	//   const url = "http://localhost:3000/";
+  const { currentUser } = useContext(UserContext);
+  //   const url = "http://localhost:3000/";
 
-	return (
-		<div className='container homePage'>
-			<h1 className='home-heading'>Welcome {currentUser.firstName}</h1>
-			<p className='home-text'>
-				There are many variations of passages of Lorem Ipsum available,
-				<br />
-				but the majority have suffered alteration in some form, by injected
-				<br /> humour, or randomised words which don't look even slightly <br />
-				believable.
-			</p>
-			{/* <span>
-        <img src={imgA} alt="man and a woman" className="imgA" width="70%" />
-      </span>
-      <span>
-        <img src={imgD} alt="man and a woman" className="imgD" width="70%" />
-      </span> */}
-			<Link to='/alltrips'>
-				<Button className='home-button-all'> All trips </Button>
-			</Link>
-			<Link to='/add-trip'>
-				<Button className='home-button-new'> Add a new trip </Button>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="container homePage">
+      <Row>
+        <h1 className="home-heading">Welcome {currentUser.firstName}</h1>
+      </Row>
+      <Row>
+        <Col md="4"></Col>
+        <Col md="4">
+          <p className="home-text">
+            Welcome to our platform. Here, you can post your trips to the
+            makolet, ask a neighbor to bring you something from teh makolet if
+            you are in potatoe-mode, through a simple point system.
+          </p>
+        </Col>
+        <Col md="4"></Col>
+      </Row>
+      <Row>
+        <Col md="4"></Col>
+        <Col md="4">
+          <h4>How does it work? </h4>
+          <p>
+            When you sign up, you are awarded 20 points. <br />
+            Every time you ask for something, you give that person point, and
+            every time you bring something to a potatoe, you earn points.
+          </p>
+        </Col>
+        <Col md="4"></Col>
+      </Row>
+      <Row>
+        <Col md="4"></Col>
+        <Col md="4">
+          <Link to="/alltrips">
+            <Button className="home-button m-4"> All trips </Button>
+          </Link>
+          <Link to="/add-trip">
+            <Button className="home-button m-4"> Add a new trip </Button>
+          </Link>
+        </Col>
+        <Col md="4"></Col>
+      </Row>
+    </div>
+  );
 }
